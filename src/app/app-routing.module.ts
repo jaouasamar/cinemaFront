ce59b728cf3f0e7ef13ec077eb480eee7bed89a3
samar jaoua <jaoua.samar@gmail.com>
2021-10-18 11:10:51 +0100

    villes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CinemaComponent } from './cinema/cinema.component';
 
 const routes: Routes = [
-  path: "/cinema",
-  component: CinemaComponent
+  {
+    path: "cinema",
+    component: CinemaComponent
+  }
 ];
 
 @NgModule({
--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CinemaComponent implements OnInit {
 
-  constructor() { }
+  public villes: Object | undefined;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get("/localhost:8070/villes").subscribe
+    (
+      data=>{
+        this.villes=data;
+
+    },
+      err=>{
+        console.log(err);
+      });
   }
 
 }
